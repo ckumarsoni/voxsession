242a9cdedfb9555b49d580877fd2d81d549c170c
ckumarsoni <100946560+ckumarsoni@users.noreply.github.com>
2026-03-22 20:11:08 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
-  PORT:           process.env.PORT           || 3000,
-  JWT_SECRET:     process.env.JWT_SECRET     || "voxsession_v4_secret_change_this",
+  PORT:           process.env.PORT           || 8080,
+  JWT_SECRET:     process.env.JWT_SECRET     || "voxsession_secret_change_this",
   ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "admin123",
   NODE_ENV:       process.env.NODE_ENV       || "production",
 };
